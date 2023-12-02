063ddbda329a264d405f6ba97b69d113d743fcd8
HugoErb <erb.hugo@yahoo.fr>
2023-12-02 14:14:35 +0100

    ajout primeng + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CreationComponent } from './creation/creation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  }, {
+    path: "creation",
+    component: CreationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
