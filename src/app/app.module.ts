e0c30f4dfcdd89e5bc5582afc19e6279705c09c8
HugoErb <erb.hugo@yahoo.fr>
2023-12-03 22:43:53 +0100

    création de toutes les pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CreationComponent } from './creation/creation.component';
+import { ModificationComponent } from './modification/modification.component';
+import { DisplayComponent } from './display/display.component';
+import { ManageAccountComponent } from './manage-account/manage-account.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ConsultationComponent } from './consultation/consultation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CreationComponent
+    CreationComponent,
+    ModificationComponent,
+    DisplayComponent,
+    ManageAccountComponent,
+    SettingsComponent,
+    ConsultationComponent
   ],
   imports: [
     BrowserModule,
