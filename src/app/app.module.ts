2d290c8367d87ee421f424e3397c0fb7a03fcd54
HugoErb <erb.hugo@yahoo.fr>
2023-12-04 22:35:58 +0100

    fix primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,7 +36,8 @@ import { DropdownModule } from 'primeng/dropdown';
     PaginatorModule,
     InputSwitchModule,
     InputNumberModule,
-    DropdownModule
+    DropdownModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
