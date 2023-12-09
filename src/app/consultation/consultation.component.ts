ecbe40cabc80bd646c7af85314a7ed90e05c411d
HugoErb <erb.hugo@yahoo.fr>
2023-12-09 15:38:16 +0100

    ajout et modif icones

--- src/app/consultation/consultation.component.ts
+++ src/app/consultation/consultation.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./consultation.component.scss']
 })
 export class ConsultationComponent {
-
+  clubsTirSportif: any = {
+    labels: ['Les Tireurs Rapides', 'Visée Parfaite', 'Les As du Pistolet'],
+    datasets: [
+        {
+            data: [540, 325, 702],
+        }
+    ]
+};
+  options = {
+    plugins: {
+        legend: {
+            labels: {
+                usePointStyle: true,
+                color: '#303030'
+            }
+        }
+    }
+};
 }
