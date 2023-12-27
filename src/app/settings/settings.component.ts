adaef6c6f8cd96e56d712fdb85024361936b8878
HugoErb <erb.hugo@yahoo.fr>
2023-12-27 16:40:50 +0100

    ajout tableau transactions

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class SettingsComponent {
   email: string = "";
+  
+  transactions: Transaction[] = [
+    { id: 1, date: '2023/06/01', montant: 19.99, typeOffre: 'Abonnement VIP', statutPaiement: 'Payé' },
+    { id: 2, date: '2023/05/01', montant: 14.99, typeOffre: 'Abonnement Pro', statutPaiement: 'Payé' },
+    { id: 3, date: '2023/04/01', montant: 14.99, typeOffre: 'Abonnement Pro', statutPaiement: 'En attente' },
+    { id: 4, date: '2023/03/01', montant: 14.99, typeOffre: 'Abonnement Pro', statutPaiement: 'Payé' },
+    { id: 5, date: '2023/02/01', montant: 7.99, typeOffre: 'Abonnement Basique', statutPaiement: 'Annulé' }
+  ];
+
+}
+
+interface Transaction {
+  id: number;
+  date: string;
+  montant: number;
+  typeOffre: string;
+  statutPaiement: string;
 }
