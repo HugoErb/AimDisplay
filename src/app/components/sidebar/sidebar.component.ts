a6b28f94c0dcf52637a5b62015bd9a050ed68ecb
HugoErb <erb.hugo@yahoo.fr>
2025-05-18 18:24:55 +0200

    maj readme + ajout sidebar et gestion route

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+// import { AuthService } from '../../services/auth.service';
+import { CommonService } from '../../services/common.service';
+
+@Component({
+    selector: 'app-sidebar',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './sidebar.component.html',
+})
+export class SidebarComponent {
+
+    constructor(
+        protected commonService: CommonService,
+        // protected authService: AuthService
+    ) { }
+
+    /**
+    * Inverse la valeur de la propriété `isCollapsed`,
+    * permettant d'afficher ou de masquer la barre latérale.
+    */
+    toggleSidebar() {
+        this.commonService.isCollapsed = !this.commonService.isCollapsed;
+    }
+}
