cccf85e84dc5a7037fc50c0c91d8effdc8d4dc36
HugoErb <erb.hugo@yahoo.fr>
2025-05-18 21:06:47 +0200

    import primeNG + maj tailwind + fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,34 +2,33 @@ import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { PrimeNG } from 'primeng/config';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet],
+	templateUrl: './app.component.html',
 })
 export class AppComponent {
-    title = 'AimDisplay';
+	title = 'AimDisplay';
 
-    constructor(private translateService: TranslateService) {}
+	constructor(private config: PrimeNG, private translateService: TranslateService) {}
 
-    ngOnInit() {
-        this.translateService.addLangs(['fr']);
-        this.translateService.setDefaultLang('fr');
-        this.translate('fr');
+	ngOnInit() {
+		this.translateService.addLangs(['fr']);
+		this.translateService.setDefaultLang('fr');
+		this.translate('fr');
 
-        let theme = localStorage.getItem('theme');
-        if (theme !== 'dark' && theme !== 'light') {
-            localStorage.setItem('theme', 'light');
-            theme = 'light';
-        }
-    }
+		let theme = localStorage.getItem('theme');
+		if (theme !== 'dark' && theme !== 'light') {
+			localStorage.setItem('theme', 'light');
+			theme = 'light';
+		}
+	}
 
-    translate(lang: string) {
-        // this.translateService.use(lang);
-        // this.translateService
-        //   .get('primeng')
-        //   .subscribe((res) => this.config.setTranslation(res));
-    }
+	translate(lang: string) {
+		this.translateService.use(lang);
+		this.translateService.get('primeng').subscribe((res) => this.config.setTranslation(res));
+	}
 }
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,55 +1,56 @@
 import { Component } from '@angular/core';
-import { InputTextarea } from 'primeng/inputtextarea';
+import { TextareaModule } from 'primeng/textarea';
 import { FormsModule } from '@angular/forms';
 import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
-    selector: 'settings',
-    templateUrl: './settings.component.html',
-    standalone: true,
-    imports: [InputTextarea, FormsModule, TableModule],
-    styleUrls: ['./settings.component.scss'],
+	selector: 'settings',
+	templateUrl: './settings.component.html',
+	standalone: true,
+	imports: [TextareaModule, FormsModule, TableModule, InputTextModule],
+	styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent {
-    email: string = '';
+	email: string = '';
 
-    transactions: Transaction[] = [
-        {
-            id: 1,
-            date: '2023/06/01',
-            montant: 19.99,
-            typeOffre: 'Abonnement VIP',
-            statutPaiement: 'Payé',
-        },
-        {
-            id: 2,
-            date: '2023/05/01',
-            montant: 14.99,
-            typeOffre: 'Abonnement Pro',
-            statutPaiement: 'Payé',
-        },
-        {
-            id: 3,
-            date: '2023/04/01',
-            montant: 14.99,
-            typeOffre: 'Abonnement Pro',
-            statutPaiement: 'En attente',
-        },
-        {
-            id: 4,
-            date: '2023/03/01',
-            montant: 14.99,
-            typeOffre: 'Abonnement Pro',
-            statutPaiement: 'Payé',
-        },
-        {
-            id: 5,
-            date: '2023/02/01',
-            montant: 7.99,
-            typeOffre: 'Abonnement Basique',
-            statutPaiement: 'Annulé',
-        },
-    ];
+	transactions: Transaction[] = [
+		{
+			id: 1,
+			date: '2023/06/01',
+			montant: 19.99,
+			typeOffre: 'Abonnement VIP',
+			statutPaiement: 'Payé',
+		},
+		{
+			id: 2,
+			date: '2023/05/01',
+			montant: 14.99,
+			typeOffre: 'Abonnement Pro',
+			statutPaiement: 'Payé',
+		},
+		{
+			id: 3,
+			date: '2023/04/01',
+			montant: 14.99,
+			typeOffre: 'Abonnement Pro',
+			statutPaiement: 'En attente',
+		},
+		{
+			id: 4,
+			date: '2023/03/01',
+			montant: 14.99,
+			typeOffre: 'Abonnement Pro',
+			statutPaiement: 'Payé',
+		},
+		{
+			id: 5,
+			date: '2023/02/01',
+			montant: 7.99,
+			typeOffre: 'Abonnement Basique',
+			statutPaiement: 'Annulé',
+		},
+	];
 }
 
 interface Transaction {
