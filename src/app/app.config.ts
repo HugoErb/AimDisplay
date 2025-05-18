96ae080d3600f803527b2d4a80648ed2ec10819b
HugoErb <erb.hugo@yahoo.fr>
2025-05-18 21:06:47 +0200

    import primeNG + maj tailwind + fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,28 +1,57 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/aura';
+import { definePreset } from '@primeng/themes';
 
 export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
+const LaraPreset = definePreset(Lara, {
+	semantic: {
+		primary: {
+			50: '{blue.50}',
+			100: '{blue.100}',
+			200: '{blue.200}',
+			300: '{blue.300}',
+			400: '{blue.400}',
+			500: '{blue.500}',
+			600: '{blue.600}',
+			700: '{blue.700}',
+			800: '{blue.800}',
+			900: '{blue.900}',
+			950: '{blue.950}',
+		},
+	},
+});
+
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideRouter(routes),
-        provideHttpClient(),
-        provideAnimations(),
-        importProvidersFrom(
-            TranslateModule.forRoot({
-                loader: {
-                    provide: TranslateLoader,
-                    useFactory: HttpLoaderFactory,
-                    deps: [HttpClient],
-                },
-            })
-        ),
-    ],
+	providers: [
+		provideRouter(routes),
+		provideHttpClient(),
+		provideAnimations(),
+		providePrimeNG({
+			theme: {
+				preset: LaraPreset,
+				options: {
+					darkModeSelector: false || 'none',
+				},
+			},
+		}),
+		importProvidersFrom(
+			TranslateModule.forRoot({
+				loader: {
+					provide: TranslateLoader,
+					useFactory: HttpLoaderFactory,
+					deps: [HttpClient],
+				},
+			})
+		),
+	],
 };
