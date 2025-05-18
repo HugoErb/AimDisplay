9903d92732020bef61859103774c64d303f9d402
HugoErb <erb.hugo@yahoo.fr>
2025-05-18 18:24:55 +0200

    maj readme + ajout sidebar et gestion route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ModificationComponent } from './modification/modification.component';
 import { ConsultationComponent } from './consultation/consultation.component';
 import { DisplayComponent } from './display/display.component';
 import { SettingsComponent } from './settings/settings.component';
+import { BaseLayoutComponent } from './components/base-layout/base-layout.component';
 
 export const routes: Routes = [
   {
@@ -13,27 +14,33 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: 'creation',
-    component: CreationComponent,
-  },
-  {
-    path: 'modification',
-    component: ModificationComponent,
-  },
-  {
-    path: 'consultation',
-    component: ConsultationComponent,
-  },
-  {
-    path: 'display',
-    component: DisplayComponent,
-  },
-  {
-    path: 'settings',
-    component: SettingsComponent,
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'creation',
+        component: CreationComponent,
+      },
+      {
+        path: 'modification',
+        component: ModificationComponent,
+      },
+      {
+        path: 'consultation',
+        component: ConsultationComponent,
+      },
+      {
+        path: 'display',
+        component: DisplayComponent,
+      },
+      {
+        path: 'settings',
+        component: SettingsComponent,
+      },
+    ],
   },
 ];
