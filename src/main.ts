4e2ff9daed90105cb74c0489ae1eb5d11a5884f6
HugoErb <erb.hugo@yahoo.fr>
2025-05-19 16:56:30 +0200

    fix style + ajout iconify + ajout icones sidebar + modify logo + fix home page

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import 'iconify-icon';
+
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
