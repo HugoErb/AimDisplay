c40b4832aeec576cf583e110f4ac326cb3a676ec
HugoErb <erb.hugo@yahoo.fr>
2025-05-27 23:26:48 +0200

    creation des composants creation club et creation competition + modification creation en creation shooter + adapt sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CreationComponent } from './creation/creation.component';
+import { CreationShooterComponent } from './creation_shooter/creation_shooter.component';
+import { CreationCompetitionComponent } from './creation_competition/creation_competition.component';
+import { CreationClubComponent } from './creation_club/creation_club.component';
 import { ModificationComponent } from './modification/modification.component';
 import { ConsultationComponent } from './consultation/consultation.component';
 import { DisplayComponent } from './display/display.component';
@@ -8,39 +10,47 @@ import { SettingsComponent } from './settings/settings.component';
 import { BaseLayoutComponent } from './components/base-layout/base-layout.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full',
-    },
-    {
-        path: '',
-        component: BaseLayoutComponent,
-        children: [
-            {
-                path: 'home',
-                component: HomeComponent,
-            },
-            {
-                path: 'creation',
-                component: CreationComponent,
-            },
-            {
-                path: 'modification',
-                component: ModificationComponent,
-            },
-            {
-                path: 'consultation',
-                component: ConsultationComponent,
-            },
-            {
-                path: 'display',
-                component: DisplayComponent,
-            },
-            {
-                path: 'settings',
-                component: SettingsComponent,
-            },
-        ],
-    },
+	{
+		path: '',
+		redirectTo: 'home',
+		pathMatch: 'full',
+	},
+	{
+		path: '',
+		component: BaseLayoutComponent,
+		children: [
+			{
+				path: 'home',
+				component: HomeComponent,
+			},
+			{
+				path: 'creation_shooter',
+				component: CreationShooterComponent,
+			},
+			{
+				path: 'creation_competition',
+				component: CreationCompetitionComponent,
+			},
+			{
+				path: 'creation_club',
+				component: CreationClubComponent,
+			},
+			{
+				path: 'modification',
+				component: ModificationComponent,
+			},
+			{
+				path: 'consultation',
+				component: ConsultationComponent,
+			},
+			{
+				path: 'display',
+				component: DisplayComponent,
+			},
+			{
+				path: 'settings',
+				component: SettingsComponent,
+			},
+		],
+	},
 ];
