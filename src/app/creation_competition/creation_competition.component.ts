86e3b6915be81a99d562d04d33ec904695fb8114
HugoErb <erb.hugo@yahoo.fr>
2025-06-13 13:15:10 +0200

    ajout de la vérification des champs pour les compétitions et les clubs

--- src/app/creation_competition/creation_competition.component.ts
+++ src/app/creation_competition/creation_competition.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { DatePickerModule } from 'primeng/datepicker';
 import { CommonModule } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonService } from '../services/common.service';
 
 @Component({
 	selector: 'app-creation-competition',
@@ -14,9 +15,29 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CreationCompetitionComponent {
+	constructor(protected commonService: CommonService) {}
+
 	// Variables de création d'une compétition
+	@ViewChildren('inputField', { read: ElementRef }) inputFields!: QueryList<ElementRef>;
+	public inputLabelMap = new Map<string, string>();
 	competitionDate: string = '';
 	competitionName: string = '';
 	prixInscription: number = 0;
 	prixCategSup: number = 0;
+
+	/**
+	 * Permet de créer une compétition à partir des données récoltées dans les champs du formulaire.
+	 * Une phase de validation des inputs est d'abord lancé, puis, si la création réussit,
+	 * on réinitialise les champs de saisie.
+	 *
+	 * @returns {Promise<void>} Une promesse qui se résout une fois que la création est effectuée et que les
+	 * champs de saisie ont été réinitialisés en cas de succès.
+	 */
+	async createCompetition(): Promise<void> {
+		this.inputLabelMap = this.commonService.getInputLabelMap(this.inputFields);
+
+		if (await this.commonService.sendMail(this.inputLabelMap)) {
+			this.commonService.resetInputFields(this.inputFields);
+		}
+	}
 }
