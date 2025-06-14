2f99c45f8f8a19037dc41a85e70a3da00afdc8e9
HugoErb <erb.hugo@yahoo.fr>
2025-06-14 20:45:54 +0200

    gestion du dark theme dans le base layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { PrimeNG } from 'primeng/config';
-import { ThemeService } from './services/theme.service';
 
 @Component({
 	selector: 'app-root',
@@ -14,14 +13,12 @@ export class AppComponent {
 	theme: string | null = '';
 	title = 'AimDisplay';
 
-	constructor(private config: PrimeNG, private translateService: TranslateService, private themeService: ThemeService) {}
+	constructor(private config: PrimeNG, private translateService: TranslateService) {}
 
 	ngOnInit() {
 		this.translateService.addLangs(['fr']);
 		this.translateService.setDefaultLang('fr');
 		this.translate('fr');
-
-		this.themeService.getTheme();
 	}
 
 	translate(lang: string) {
--- src/app/components/base-layout/base-layout.component.ts
+++ src/app/components/base-layout/base-layout.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonService } from '../../services/common.service';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
-    selector: 'app-base-layout',
-    standalone: true,
-    imports: [RouterOutlet, SidebarComponent],
-    templateUrl: './base-layout.component.html',
+	selector: 'app-base-layout',
+	standalone: true,
+	imports: [RouterOutlet, SidebarComponent],
+	templateUrl: './base-layout.component.html',
 })
 export class BaseLayoutComponent {
-    constructor(protected commonService: CommonService) {}
+	constructor(protected commonService: CommonService, private themeService: ThemeService) {}
+
+	theme: string | null = '';
+
+	ngOnInit() {
+		this.themeService.getTheme();
+	}
 }
