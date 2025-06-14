22da9685316b48dc090222e7b76e4b8d43ff74a6
HugoErb <erb.hugo@yahoo.fr>
2025-06-14 21:14:05 +0200

    création de méthode différentes pour la gestion des champs des formulaires + vérif dans register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
@@ -21,6 +21,10 @@ export class RegisterComponent {
 		protected commonService: CommonService // protected authService: AuthService
 	) {}
 
+	// Variables de création d'un tireur
+	@ViewChildren('inputField', { read: ElementRef }) inputFields!: QueryList<ElementRef>;
+	public inputLabelMap = new Map<string, string>();
+
 	/**
 	 * Cette méthode vérifie si l'email et le mot de passe sont renseignés,
 	 * puis appelle le service d'authentification pour se connecter avec ces informations.
@@ -28,4 +32,20 @@ export class RegisterComponent {
 	// login(): void {
 	// 	if (this.email && this.password) this.authService.signIn(this.email, this.password);
 	// }
+
+	/**
+	 * Permet de créer un nouveau compte à partir des données récoltées dans les champs du formulaire.
+	 * Une phase de validation des inputs est d'abord lancée, puis, si la création réussit,
+	 * on redirige vers la page de login.
+	 *
+	 * @returns {Promise<void>} Une promesse qui se résout une fois que la création est effectuée et que les
+	 * champs de saisie ont été réinitialisés en cas de succès.
+	 */
+	async createAccount(): Promise<void> {
+		this.inputLabelMap = this.commonService.getInputLabelMap(this.inputFields);
+
+		if (await this.commonService.createAccount(this.inputLabelMap)) {
+			this.commonService.redirectTo("login");
+		}
+	}
 }
