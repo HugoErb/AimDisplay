3300cf91b272ca42bf62e37a32b4ea22f3d0ebaf
HugoErb <erb.hugo@yahoo.fr>
2025-08-02 00:40:01 +0200

    ajout firebase et service d'authent

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+	production: false,
+	firebase: {
+		apiKey: 'AIzaSyCxVUfTUkzqI6x9u_1zMCVspdtjpk9vGhE',
+		authDomain: 'aim-display-7e3aa.firebaseapp.com',
+		projectId: 'aim-display-7e3aa',
+		storageBucket: 'aim-display-7e3aa.firebasestorage.app',
+		messagingSenderId: '302240618482',
+		appId: '1:302240618482:web:365da6a7c8bf99a27bfd6b',
+		measurementId: 'G-30QK8PGLWX',
+	},
+};
