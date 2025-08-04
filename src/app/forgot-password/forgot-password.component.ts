a542fd62731ae759044646af3bb6c9a1346f39a1
HugoErb <erb.hugo@yahoo.fr>
2025-08-04 20:30:28 +0200

    implémentation de l'oubli de mot de passe

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { CommonService } from '../services/common.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
 	selector: 'app-forgot-password',
@@ -12,9 +13,29 @@ import { CommonService } from '../services/common.service';
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ForgotPasswordComponent {
+	@ViewChildren('inputField', { read: ElementRef }) inputFields!: QueryList<ElementRef>;
+	public inputLabelMap = new Map<string, string>();
+	passwordFieldType: string = 'password';
 	email: string = '';
+	isLoading: boolean = false;
 
-	constructor(
-		protected commonService: CommonService // protected authService: AuthService
-	) {}
+	constructor(protected commonService: CommonService, protected authService: AuthService) {}
+
+	/**
+	 * Permet d'envoyer un lien de réinitialisation de mot de passe au mail renseigné dans le formulaire.
+	 * Une phase de validation des inputs est d'abord lancée.
+	 *
+	 * @returns {Promise<void>} Une promesse qui se résout une fois que le mail est envoyé.
+	 */
+	async sendPasswordResetEmail(): Promise<void> {
+		this.isLoading = true;
+		this.inputLabelMap = this.commonService.getInputLabelMap(this.inputFields);
+		const areInputsValid = await this.commonService.validateInputs(this.inputLabelMap, false);
+		if (areInputsValid) {
+			await this.authService.sendPasswordResetEmail(this.email.trim());
+            this.commonService.showSwal('E-mail de réinitialisation envoyé !','Veuillez suivre les indications de l\'email que vous avez reçu afin de réinitialiser votre mot de passe. N\'oubliez pas de vérifier vos spam !','success', false);
+			this.commonService.redirectTo('login');
+			this.isLoading = false;
+		}
+	}
 }
