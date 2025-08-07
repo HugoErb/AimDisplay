66d68f8fefbf4e28a1a976b3f0afcdac9009515a
HugoErb <erb.hugo@yahoo.fr>
2025-08-07 18:56:12 +0200

    passage à supabase, delete firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,15 +2,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const environment = {
 	production: false,
-	firebase: {
-		apiKey: 'AIzaSyCxVUfTUkzqI6x9u_1zMCVspdtjpk9vGhE',
-		authDomain: 'aim-display-7e3aa.firebaseapp.com',
-		projectId: 'aim-display-7e3aa',
-		storageBucket: 'aim-display-7e3aa.firebasestorage.app',
-		messagingSenderId: '302240618482',
-		appId: '1:302240618482:web:365da6a7c8bf99a27bfd6b',
-		measurementId: 'G-30QK8PGLWX',
-	},
 	supabase: {
 		url: 'https://tnqpedwmtribtocyjwpf.supabase.co',
 		anonKey:
