2256a69548e165381906524e79aeecd98f278ef9
HugoErb <erb.hugo@yahoo.fr>
2025-08-23 23:18:35 +0200

    reformat files + delete firebase init

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,4 @@ import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import 'iconify-icon';
 
-
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
