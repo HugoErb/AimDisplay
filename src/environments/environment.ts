64775948ba983c564c6ae9718627aa28f8c72c0e
HugoErb <erb.hugo@yahoo.fr>
2026-03-18 14:46:46 +0100

    maj projet supabase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,8 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const environment = {
 	production: false,
 	supabase: {
-		url: 'https://tnqpedwmtribtocyjwpf.supabase.co',
+		url: 'https://wzgomoetshumurbzuaku.supabase.co',
 		anonKey:
-			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRucXBlZHdtdHJpYnRvY3lqd3BmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1MDcxMDgsImV4cCI6MjA3MDA4MzEwOH0.xtENN1VRIppmLe5DUEe4-wwwIWIdRJb11cI0G5fwqhM',
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6Z29tb2V0c2h1bXVyYnp1YWt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MzgyNjUsImV4cCI6MjA4OTQxNDI2NX0.uHQ48lYXAP7g5pYFDjl0BL2hQhV9l_d14AT8uFH9vyc',
 	},
 };
