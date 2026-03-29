350a688e4dbb80bb56a4056e08e15cbb89ab467a
HugoErb <erb.hugo@yahoo.fr>
2026-03-29 20:13:09 +0200

    refacto composant redirect link

--- src/app/components/redirect-link/redirect-link.component.ts
+++ src/app/components/redirect-link/redirect-link.component.ts
@@ -6,37 +6,7 @@ import { CommonService } from '../../services/common.service';
 	selector: 'app-redirect-link',
 	standalone: true,
 	imports: [CommonModule],
-	template: `
-		<span (click)="redirectTo()" class="redirect-link" [class.force-light]="forceLight">
-			<ng-content></ng-content>
-		</span>
-	`,
-	styles: [
-		`
-			.redirect-link {
-				color: var(--color-prime-blue);
-				font-weight: 500;
-				cursor: pointer;
-				transition: color 0.2s;
-			}
-
-			.redirect-link:hover {
-				color: var(--color-hover-prime-blue);
-				text-decoration: underline;
-			}
-
-			/* On n'applique le style sombre que si on n'est PAS en force-light */
-			:host-context(.dark) .redirect-link:not(.force-light),
-			:host(.dark) .redirect-link:not(.force-light) {
-				color: var(--color-prime-blue-light);
-			}
-
-			:host-context(.dark) .redirect-link:not(.force-light):hover,
-			:host(.dark) .redirect-link:not(.force-light):hover {
-				color: var(--color-hover-prime-blue-light);
-			}
-		`,
-	],
+	templateUrl: './redirect-link.component.html',
 })
 export class RedirectLinkComponent {
 	@Input() target: string = '';
