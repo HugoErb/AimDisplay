9ce8c5532fe498ff14700dac8771639143d82b15
HugoErb <erb.hugo@yahoo.fr>
2026-03-29 16:02:17 +0200

    ajout elements home page + modif couleur section header icone

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonService } from '../services/common.service';
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
 	imports: [],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 	templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+	constructor(protected commonService: CommonService) {}
+}
