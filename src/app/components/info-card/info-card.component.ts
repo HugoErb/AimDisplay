6529d4821ea24d0c674831e7b99fc5dad2d22a78
HugoErb <erb.hugo@yahoo.fr>
2026-04-01 19:43:29 +0200

    fix taille info card

--- src/app/components/info-card/info-card.component.ts
+++ src/app/components/info-card/info-card.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 	standalone: true,
 	imports: [CommonModule],
 	templateUrl: './info-card.component.html',
+	styles: [`:host { display: block; height: 100%; }`],
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class InfoCardComponent {
